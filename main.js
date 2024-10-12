49a06538b7df851583ae2e3abca8976980aca2a8
KKooLNoI <chayangkool.sr@gmail.com>
2024-10-13 01:26:49 +0700

    Final Credit

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ new Swiper('.card-wrapper', {
     spaceBetween: 30,
 
 
-    // If we need pagination
+    
     pagination: {
       el: '.swiper-pagination',
     },
   
-    // Navigation arrows
+    
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
@@ -27,28 +27,28 @@ new Swiper('.card-wrapper', {
     }
   });
 
-// Select all buttons with the class 'card-link'
+
 const cardLinks = document.querySelectorAll('.card-link');
 const popups = document.querySelectorAll('.popup-overlay');
 const closeButtons = document.querySelectorAll('.close-btn');
 
-// Loop through each card link and add an event listener
+
 cardLinks.forEach(link => {
     link.addEventListener('click', (e) => {
         const popupId = link.getAttribute('data-popup');
         const popup = document.getElementById(popupId);
-        popup.style.display = 'flex'; // Show the specific popup
+        popup.style.display = 'flex'; 
     });
 });
 
-// Add event listeners to close the popups
+
 closeButtons.forEach(button => {
     button.addEventListener('click', () => {
         button.closest('.popup-overlay').style.display = 'none';
     });
 });
 
-// Close the popup if the user clicks outside the popup content
+
 window.addEventListener('click', (e) => {
     if (e.target.classList.contains('popup-overlay')) {
         e.target.style.display = 'none';
